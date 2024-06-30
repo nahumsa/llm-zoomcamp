45e794347218b3453d63eb1a2fea250ddbf4be7d
Nahum Sá <nahumsaa@gmail.com>
2024-06-30 15:25:23 -0300

    feat: finishing up HW 01

--- hw_01/README.md
+++ hw_01/README.md
@@ -1,5 +1,12 @@
 # Homework 01
 
+In order to run the python files you need to run:
+
+```bash
+pip install pipenv
+pipenv install
+```
+
 ## Q1 - Running Elastic
 
 You just need to run `sh start_elastic.sh`, then run the curl command `curl localhost:9200` to find the `version.build_hash`:
@@ -8,4 +15,30 @@ You just need to run `sh start_elastic.sh`, then run the curl command `curl loca
 42f05b9372a9a4a470db3b52817899b99a76ee73
 ```
 
-## Q2 - Running Elastic
+## Q2 - Indexing data
+
+To index data you need to use `.index`.
+
+## Q3 - Searching
+
+You just need to run `searching.py`. The score is: `84.05`
+
+## Q4 - Filtering
+
+You just need to run `searching.py`. The 3rd result is: `How do I copy files from a different folder into docker container’s working directory?`
+
+## Q5 - Building a prompt
+
+You need to run `prompt.py`, then the answer is:
+
+```bash
+1462
+```
+
+## Q6 - Tokens
+
+You need to run `prompt.py`, then the answer is:
+
+```bash
+322
+```
--- hw_01/prompt.py
+++ hw_01/prompt.py
@@ -1,9 +1,50 @@
-from searching import es_query
+import tiktoken
 
-question = "How do I execute a command in a running docker container?"
-filter_course = "machine-learning-zoomcamp"
-print(
-    es_query(question, filter_course=filter_course)["hits"][2]["_source"][  # type: ignore
-        "question"
-    ],
-)
+from searching import es_query
+from pydantic import BaseModel
+
+
+class ContextExample(BaseModel):
+    question: str
+    answer: str
+
+    def generate_example(self) -> str:
+        return f"""
+Q: {self.question}
+A: {self.answer}
+"""
+
+
+def generate_context() -> list[str]:
+    question = "How do I execute a command in a running docker container?"
+    filter_course = "machine-learning-zoomcamp"
+    context_response = []
+    for hit in es_query(question, filter_course=filter_course, size=3)["hits"]:
+        context_response.append(
+            ContextExample(
+                question=question, answer=hit["_source"]["text"]
+            ).generate_example()
+        )
+    return context_response
+
+
+def generate_prompt(question: str, context: list[str]) -> str:
+    all_context = "\n\n".join(context)
+    return f"""
+You're a course teaching assistant. Answer the QUESTION based on the CONTEXT from the FAQ database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT:
+{all_context}
+""".strip()
+
+
+if __name__ == "__main__":
+    question = "How do I execute a command in a running docker container?"
+    prompt = generate_prompt(question=question, context=generate_context())
+    encoding = tiktoken.encoding_for_model("gpt-4o")
+    print(prompt)
+    print(f"length of the prompt: {len(prompt)}")
+    print(f"# of tokens: {len(encoding.encode(prompt))}")
